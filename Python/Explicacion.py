8fa336ba5efdd1031265d3e5e83fab769cde7ba0
CromoMx <david.raw172@gmail.com>
2020-07-28 23:38:18 -0500

    Se complemento la explicacion de Python

--- Python/Explicacion.py
+++ Python/Explicacion.py
@@ -1,8 +1,77 @@
-#Tipos de datos
-#Entero
-num = 20
-print (num)
-#String
-texto = "abcd"
-#float
-dec = 20.5
+"""
+Notas del lenguaje:
+1 En este lenguaje todas las salidas deben de 
+ser string
+2 No hay ;
+3 Es MUY importante tener alineado el codigo
+4 No exite "switch", "do while"
+"""
+#Clase principal
+def Main():
+    print("Menu")
+    print("1)Tipos de datos")
+    print("2)Condiciones")
+    print("3)Ciclos")
+    print("4)Vectores")
+    opc = int(input("Digame su opc: "))
+    Menu(opc)
+
+#Menu
+def Menu(p_opc):
+    #Simula un switch
+    if p_opc < 5 and p_opc > 0:
+        if p_opc == 1:
+            Datos()
+        if p_opc == 2:
+            Condiciones()
+        if p_opc == 3:
+            Ciclos()
+        if p_opc == 4:
+            Vectores()
+    #Se simula el default de un switch con else
+    else:
+        print("Opcion no valida")
+
+#Tipos de datos e impresion
+def Datos():
+    #Entero
+    num = 20
+    #String
+    texto = "abcd"
+    #float
+    dec = 20.5
+    #Tipos de impresion
+    print("\nTexto: "+texto+"\nEntero: "+str(num)+"\nFloat: "+str(dec))
+#Ciclos
+def Condiciones():
+    #If
+    opc = int(input("Cuanto es 3+3\n"))
+    if opc == 6:
+        print("Simon xd\n")
+    else:
+        print("nelson xd\n")
+#Ciclos
+def Ciclos():
+    #for
+    opc = int(input("Contar hasta: "))
+    for i in range(opc):
+        print(i+1)
+    #while
+    res = int(input("5+5 "))
+    while res!=10:
+        print("no")
+        res = int(input("5+5 "))
+    print("Siiiuuuu")
+    #simulacion de do while
+    ban=True
+    while ban==True:
+        num = int(input("Ingrese un numero menor a 10: "))
+        if num<10:
+            ban=False
+    print("correcto")
+#Vectores
+def Vectores():
+    print("vec")
+
+#Correr el programa desde la clase principal
+Main()
